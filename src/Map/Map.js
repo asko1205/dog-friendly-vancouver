870f678d06d2a1e26fd067de425912bdf6728c19
Alex Skodyn <alex@schoolzilla.com>
2018-03-25 21:14:30 -0700

    Initial pass at styling. Adding logo.

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -12,7 +12,7 @@ const Map = compose(
         googleMapURL:
             "https://maps.googleapis.com/maps/api/js?key=AIzaSyDzZurSP3E9szvUVtvwWlgcg-s7iF82SIM&v=3.exp&libraries=geometry,drawing,places",
         loadingElement: <div style={{ height: `100%` }} />,
-        containerElement: <div style={{ height: `400px` }} />,
+        containerElement: <div style={{ height: `40rem` }} />,
         mapElement: <div style={{ height: `100%` }} />
     }),
     withScriptjs,
